2227b9ee0da169b89bc2cbb0febc24c82ddb09a6
ivanrustic <ivan.rustic@gmail.com>
2020-12-22 12:13:25 +0100

    Commit!

--- test/src/panorama/EmptyPanorama.test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import { EmptyPanorama } from '../../../src/panorama/EmptyPanorama';
-
-test('Load Event', t => {
-    const panorama = new EmptyPanorama();
-    panorama.load();
-    t.true(panorama.loaded);
-});
